1fb433121e58b130a14ddf83d647372cc03a024b
Isaac Abitey <abiteyisaac2902gmail.com>
2021-11-21 11:10:31 -0800

    updated file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,5 @@
 
 
 def no_c(my_string):
-    my_string = ''.join(char)
-    for char in my_string:
-        if char not in 'Cc':
-            return my_string
+    my_string = ''.join(char for char in my_string if char not in 'Cc')
+    return my_string
